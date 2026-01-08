dad7aa271b93ea047a7f890c5c48a6d2933105fa
natesan750 <168168163+natesan750@users.noreply.github.com>
2026-01-08 11:50:11 +0800

    CI workflows

--- backend/test/tasks.test.js
+++ backend/test/tasks.test.js
@@ -3,6 +3,7 @@ const { server, app } = require("../index.js");
 const mongoose = require("mongoose");
 
 describe("GET api/tasks", () => {
+
   it("it should return 200 ok", async () => {
     const res = await request(app).get("/api/tasks");
     expect(res.statusCode).toBe(200);
@@ -11,7 +12,8 @@ describe("GET api/tasks", () => {
   it("it should return object and task property ok", async () => {
     const res = await request(app).get("/api/tasks");
     expect(typeof res.body).toBe("object");
-    expect(res.body).toHaveProperty("tasks")
+    expect(res.body).toHaveProperty("tasks");
+    console.log(res.body.tasks, "Data seeded");
   });
 });
 
